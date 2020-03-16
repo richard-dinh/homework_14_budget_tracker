aaaed23100a34da7dce5f7c8b8cb19fca10f34b1
richard-dinh <richarddinh@gmail.com>
2020-03-16 15:44:02 -0700

    fixed evn config

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+require('dotenv').config()
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-require('dotenv')
 
 const app = express();
 
